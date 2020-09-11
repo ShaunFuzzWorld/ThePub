7b2357d6be22efa49672c9ecca95094e87928fd2
weigela <andy.weigel@entrustdatacard.com>
2020-09-11 09:06:57 -0500

    committing Hello World

---
+++ HelloWorld/src/main/java/aw/helloworld/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author Andy Weigel
+ *email: andrewdweigel@gmail.com
+ *date: 9/11/20
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.mthree.helloworld; 
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Andy!");
+    }
+}
